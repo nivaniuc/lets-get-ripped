1482c75bb0baffce0dda4071eed4158f992cb84f
ant <nivaniucc@gmail.com>
2021-05-07 21:47:18 -0400

    Figured out the Cluster issue I was having (I think?) Time to move onto routes and models!

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(logger("dev"));
 
 // TODO: WORK ON MONGO STUFF FOR SERVER
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
 
 // TODO: WORK ON ROUTES
 
