583ba886ef08b898819690b03d8c2808837cb187
ant <nivaniucc@gmail.com>
2021-05-07 23:31:50 -0400

    I realized the thing tripping me up was that autosave wasnt enabled...at least things with add/commit properly now.

--- models/workout.js
+++ models/workout.js
@@ -7,10 +7,15 @@ var WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
+    exercises: Array,
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
 });
 
 var Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.export = Workout;
+module.exports = Workout;
 
 //Not super duper sure how well this will work, if there are any errors, refer back to this comment (From Past-Self)
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,29 @@
 var db = require("../models");
-
+//get the workout
 module.exports = app => {
-    app.get("/api/workout", (req,res) => {
+    app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
-        .then(dbWorkout);
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-    })
+        .sort({ day: 1})
+        .then(dbWorkout =>{
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        });
 });
 
+//This is supposed to add an exercise 
+app.put("/api/workouts/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    console.log(req.body.duration);
+    db.Workout.findByIdAndUpdate({
+      _id: req.params.id
+    }, {
+      $push: { exercises: req.body },
+      $inc: { totalDuration: req.body.duration }
+    })
+    .then(dbWorkout => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
 //I cant figure out what the error is on this page right now and its honestly kind of driving me mad. Please Clean up (- Past Self)
